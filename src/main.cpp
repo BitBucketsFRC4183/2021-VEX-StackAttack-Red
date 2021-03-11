e7a03a6925536fa61270ac6214acd7c21b5870cf
Elias Post <elias@postfamilydigital.net>
2021-03-10 19:30:25 -0700

    Drive forward at 50%

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,11 @@
 /*----------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// Drivetrain           drivetrain    1, 2, 3, 4      
+// Vision5              vision        5               
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -18,4 +23,6 @@ int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
   
+  Drivetrain.setDriveVelocity(50,percent);
+  Drivetrain.drive(forward);
 }
