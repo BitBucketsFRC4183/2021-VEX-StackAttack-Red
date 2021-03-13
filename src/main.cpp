60d6f45fc92ea36c19ae8149747ebff504d76e4c
Elias Post <elias@postfamilydigital.net>
2021-03-12 17:59:49 -0700

    Drive and Turn

--- src/main.cpp
+++ src/main.cpp
@@ -10,19 +10,51 @@
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// Drivetrain           drivetrain    1, 2, 3, 4      
-// Vision5              vision        5               
+// Controller1          controller
+// Drivetrain           drivetrain    1, 2, 3, 4
+// Vision5              vision        5
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
 
 using namespace vex;
 
+float DEADBAND_F = 1.0f;
+float JOYSTICK_PERCENTAGE_CONVERSION_F = 1.0f;
+
+
+void driveRobot() {
+  if (Axis1 >= DEADBAND_F) {
+    Drivetrain.setDriveVelocity(Axis1 * JOYSTICK_PERCENTAGE_CONVERSION_F, percent);
+    Drivetrain.drive(forward);
+
+  } else if (Axis1 <= -DEADBAND_F) {
+    Drivetrain.setDriveVelocity(Axis1 * JOYSTICK_PERCENTAGE_CONVERSION_F, percent);
+    Drivetrain.drive(reverse);
+
+  } else {
+    Drivetrain.stop();
+  }
+}
+
+void turnRobot() {
+  if (Axis2 >= DEADBAND_F) {
+    Drivetrain.setTurnVelocity(Axis2 * JOYSTICK_PERCENTAGE_CONVERSION_F, percent);
+    Drivetrain.turn(left);
+
+  } else if (Axis2 <= -DEADBAND_F) {
+    Drivetrain.setTurnVelocity(Axis2 * JOYSTICK_PERCENTAGE_CONVERSION_F, percent);
+    Drivetrain.turn(right);
+
+  } else {
+    Drivetrain.stop();
+  }
+}
+
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-  
-  Drivetrain.setDriveVelocity(50,percent);
-  Drivetrain.drive(forward);
+
+  Controller1.Axis1.changed(driveRobot);
+  Controller1.Axis2.changed(turnRobot);
 }
