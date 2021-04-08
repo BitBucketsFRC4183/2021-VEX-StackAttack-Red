baa3098702ffc365ddc6b85c9908e9642e21f3d9
Elias Post <elias@postfamilydigital.net>
2021-04-07 18:37:17 -0700

    Possibly smoother arm movement

--- src/main.cpp
+++ src/main.cpp
@@ -100,10 +100,10 @@ void turnRobot() {
 
 void moveArm() {
   if (Controller1.Axis2.position(percent) >= DEADBAND_F) {
-    ArmMotor.spinToPosition(ArmMotor.position(degrees) + (Controller1.Axis2.position(percent) * armSpeed), degrees);
+    ArmMotor.spinToPosition(ArmMotor.position(degrees) + (Controller1.Axis2.position(percent) * armSpeed), degrees, false);
 
   } else if (Controller1.Axis2.position(percent) <= -DEADBAND_F) {
-    ArmMotor.spinToPosition(ArmMotor.position(degrees) + (Controller1.Axis2.position(percent) *  armSpeed), degrees);
+    ArmMotor.spinToPosition(ArmMotor.position(degrees) + (Controller1.Axis2.position(percent) *  armSpeed), degrees, false);
   }
 }
 
