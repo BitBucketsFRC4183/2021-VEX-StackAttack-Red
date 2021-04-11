b3d230402a5433a51823ec15bff2bc0150e800ae
Elias Post <epost@bitbuckets.org>
2021-04-11 16:46:02 -0700

    Auto stash before merge of "main" and "origin/main"

--- src/main.cpp
+++ src/main.cpp
@@ -105,17 +105,14 @@ void turnRobot() {
 
 void moveArm() {
   if (Controller1.ButtonL2.pressing()) {
-    std::cout<<"Spin arm forward"<<std::endl;
 
     ArmMotor.setVelocity(armSpeed, percent);
     ArmMotor.spin(forward);
 
   } else if (Controller1.ButtonR2.pressing()) {
-    std::cout<<"Spin arm backward"<<std::endl;
     ArmMotor.setVelocity(armSpeed, percent);
     ArmMotor.spin(reverse);
   } else {
-    std::cout<<"Stop arm"<<std::endl;
     ArmMotor.stop(hold);
   }
 }
